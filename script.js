54c0190dd6583ba27230caec918931f8fd3755f9
Nelson <nelson.mieszkalski@gmail.com>
2022-01-17 09:33:41 -0500

    Added click-events for the number pad

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Constants:
+current = document.querySelector(".current");
+
+
 // Functions to Calculate
 
 function add(x, y) {
@@ -27,3 +31,71 @@ function operate(operator, x, y) {
         return divide(x,y)
     }
 };
+
+
+// Click Events:
+const nine = document.getElementById("nine");
+nine.addEventListener("click", () => {
+    current.textContent += "9";
+});
+
+const eight = document.getElementById("eight");
+eight.addEventListener("click", () => {
+    current.textContent += "8";
+});
+
+const seven = document.getElementById("seven");
+seven.addEventListener("click", () => {
+    current.textContent += "7";
+});
+
+const six = document.getElementById("six");
+six.addEventListener("click", () => {
+    current.textContent += "6";
+});
+
+const five = document.getElementById("five");
+five.addEventListener("click", () => {
+    current.textContent += "5";
+});
+
+const four = document.getElementById("four");
+four.addEventListener("click", () => {
+    current.textContent += "4";
+});
+
+const three = document.getElementById("three");
+three.addEventListener("click", () => {
+    current.textContent += "3";
+});
+
+const two = document.getElementById("two");
+two.addEventListener("click", () => {
+    current.textContent += "2";
+});
+
+const one = document.getElementById("one");
+one.addEventListener("click", () => {
+    current.textContent += "1";
+});
+
+const zero = document.getElementById("zero");
+zero.addEventListener("click", () => {
+    if (!current.textContent.startsWith("0") && current.textContent != "") {
+        current.textContent += "0";}
+        }
+); 
+
+// Operator Click Events
+
+const addition = document.getElementById("addition");
+
+const subtractor = document.getElementById("subtractor");
+
+const multiplier = document.getElementById("multiplier");
+
+const divider = document.getElementById("divider");
+
+const decimal = document.getElementById("decimal");
+
+const equality = document.getElementById("equality");
